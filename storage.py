5efad9a58fb0ce351ef5eafa698605ce5c0d0313
Kv1lon <skrypnyk.michael@gmail.com>
2024-05-18 12:54:43 +0300

    simple temp storage

---
+++ storage.py
@@ -0,0 +1,43 @@
+import csv
+import tempfile
+
+
+class TempStorage:
+    def __init__(self):
+        self._file = tempfile.NamedTemporaryFile(mode='w+', delete=True, prefix='smaug_', suffix='.csv', newline='')
+        self._writer = None
+
+    def save(self, data: dict, timestamp):
+        timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')
+        if not self._writer:
+            self._writer = csv.DictWriter(self._file, fieldnames=['timestamp', *data.keys()])
+            self._writer.writeheader()
+        self._writer.writerow({'timestamp': timestamp_str, **data})
+        self._file.flush()
+
+    def get(self, timestamp):
+        timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')
+        with open(self._file.name, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                print(row.get('timestamp'), timestamp_str)
+                print(row.get('timestamp')== timestamp_str)
+                if row.get('timestamp') == timestamp_str:
+                    return row
+        return None
+
+    def get_all(self):
+        with open(self._file.name, 'r') as f:
+            return list(csv.DictReader(f))
+
+    def delete(self, timestamp):
+        timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')
+        data = self.get_all()
+        data = [row for row in data if row.get('timestamp') != timestamp_str]
+        with open(self._file.name, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=['timestamp', *data[0].keys()])
+            writer.writeheader()
+            writer.writerows(data)
+
+    def __del__(self):
+        self._file.close()
