e3d92b7c393dad148b4059148c818cbef73821df
Kv1lon <skrypnyk.michael@gmail.com>
2024-05-19 16:01:18 +0300

    csv -> pickle

--- storage.py
+++ storage.py
@@ -1,45 +1,63 @@
-import csv
+import pickle
 import tempfile
+from pickle import UnpicklingError
 
 from metrics import Metric
 
 
 class TempStorage:
-    def __init__(self):
-        self._file = tempfile.NamedTemporaryFile(mode='w+', delete=True, prefix='smaug_', suffix='.csv', newline='')
-        self._writer = None
+    def __init__(self, name=None, max_size=1000):
+        prefix = 'smaug_' if not name else f'smaug_{name}_'
+        self._file = tempfile.NamedTemporaryFile(mode='wb+', delete=False, prefix=prefix, suffix='.pkl')
+        self.max_size = max_size * 1024 # in kilobytes
 
     def save_record(self, data: Metric):
-        if not self._writer:
-            self._writer = csv.DictWriter(self._file, fieldnames=data.keys())
-            self._writer.writeheader()
-        self._writer.writerow(data.to_dict())
+        pickle.dump(data.to_bytes(), self._file)
         self._file.flush()
 
     def get_record(self, epoch):
-        with open(self._file.name, 'r') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                if int(row.get('epoch')) == epoch:
-                    return Metric.from_dict(row)
+        self._file.seek(0)
+        while True:
+            try:
+                record = pickle.load(self._file)
+                if record.get('epoch') == epoch:
+                    return Metric.from_dict(record)
+            except EOFError:
+                break
         return None
 
     def get_last_records(self, tail=0) -> list[Metric]:
-        with open(self._file.name, 'r') as f:
-            reader = csv.DictReader(f)
-            data = list(reader)
-            return [Metric.from_dict(row) for row in data[-tail:]]
+        self._file.seek(0)
+        records = []
+        while True:
+            try:
+                record = pickle.load(self._file)
+                records.append(Metric.from_bytes(record))
+            except (EOFError, UnpicklingError):
+                break
+        return records[-tail:]
 
     def delete_record(self, epoch):
-        with open(self._file.name, 'r') as f:
-            reader = csv.DictReader(f)
-            data = list(reader)
-        with open(self._file.name, 'w') as f:
-            writer = csv.DictWriter(f, fieldnames=data[0].keys())
-            writer.writeheader()
-            for row in data:
-                if int(row.get('epoch')) != epoch:
-                    writer.writerow(row)
+        self._file.seek(0)
+        records = []
+        while True:
+            try:
+                record = pickle.load(self._file)
+                if record.get('epoch') != epoch:
+                    records.append(record)
+            except EOFError:
+                break
+        self._file.seek(0)
+        self._file.truncate()
+        for record in records:
+            pickle.dump(record, self._file)
+        self._file.flush()
 
     def __del__(self):
         self._file.close()
+
+
+class BatchTempStorage(dict):
+    def __missing__(self, key) -> TempStorage:
+        self[key] = TempStorage(key)
+        return self[key]
