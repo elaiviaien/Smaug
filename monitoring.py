96ac6880b77c5aa6d080048ad31b63f78fc9a945
Kv1lon <skrypnyk.michael@gmail.com>
2024-05-19 16:02:00 +0300

    refactored

--- monitoring.py
+++ monitoring.py
@@ -1,34 +1,56 @@
-import subprocess
-import time
 import os
-from collections import deque
+import subprocess
 import threading
-import fnmatch
+import time
+from abc import abstractmethod, ABC
+
+from metrics import Metric
+from storage import BatchTempStorage
 
 
-class Monitor:
+class Monitor(ABC):
     def __init__(self):
         self.start_time = time.time()
-        self.stop_flag = False
-        self.time_point = 0.01  # 0.01 seconds
-        store_time_points = 6000  # 60 seconds
-        self.cpu_usage_values = deque(maxlen=store_time_points)
-        self.cpu_usage_thread = threading.Thread(target=self._calculate_cpu_usage)
-        self.cpu_usage_thread.daemon = True
-        self.cpu_usage_thread.start()
 
-    def _calculate_cpu_usage(self):
 
+class LiveMonitor(Monitor):
+    def __init__(self):
+        self.stop_flag = None
+        self.time_point = 0.1  # 0.1 seconds
+        self.temp_storages = BatchTempStorage()
+        self.collect_data_thread = threading.Thread(target=self._save_stats_periodically)
+        self.collect_data_thread.start()
+        super().__init__()
+
+    def _save_stats_periodically(self):
         while not self.stop_flag:
-            cpu_times1 = self._get_cpu_times()
-            time.sleep(self.time_point)
-            cpu_times2 = self._get_cpu_times()
-            cpu_delta = [t2 - t1 for t1, t2 in zip(cpu_times1, cpu_times2)]
-            total_time = sum(cpu_delta)
-            idle_time_arr_n = 3
-            idle_time = cpu_delta[idle_time_arr_n]
-            cpu_usage = (total_time - idle_time) / total_time * 100
-            self.cpu_usage_values.append(cpu_usage)
+            records = self.record_stats()
+            for record in records:
+                self.temp_storages[record.name].save_record(record)
+                time.sleep(self.time_point)
+
+    @abstractmethod
+    def record_stats(self) -> list[Metric]:
+        pass
+
+    def stop(self):
+        self.stop_flag = True
+
+    def is_thread_alive(self):
+        return self.collect_data_thread.is_alive()
+
+
+class CPUMonitor(LiveMonitor):
+    def record_stats(self):
+        cpu_times1 = self._get_cpu_times()
+        time.sleep(self.time_point)
+        cpu_times2 = self._get_cpu_times()
+        cpu_delta = [t2 - t1 for t1, t2 in zip(cpu_times1, cpu_times2)]
+        total_time = sum(cpu_delta)
+        idle_time_arr_n = 3
+        idle_time = cpu_delta[idle_time_arr_n]
+        cpu_usage = (total_time - idle_time) / total_time * 100 if total_time != 0 else 0
+        return [Metric('cpu_usage', cpu_usage, int(time.time()))]
 
     def _get_cpu_times(self):
         with open('/proc/stat', 'r') as file:
@@ -36,8 +58,12 @@ class Monitor:
         return list(map(int, cpu_line.split()[1:]))
 
     def get_cpu_usage(self):
-        return sum(self.cpu_usage_values) / len(self.cpu_usage_values)
+        records = self.temp_storages['cpu_usage'].get_last_records()
+        values = [float(record.value) for record in records]
+        return sum(values) / len(values) if values else 0
 
+
+class MemoryMonitor(LiveMonitor):
     def get_virtual_memory_usage(self):
         with open('/proc/meminfo', 'r') as mem:
             lines = mem.readlines()
@@ -62,6 +88,14 @@ class Monitor:
         swap_usage = f'{used_swap}/{total_swap} kB ({used_swap / total_swap * 100:.2f}%)' if total_swap != 0 else 'No swap space'
         return swap_usage
 
+    def record_stats(self):
+        virtual_memory_usage = self.get_virtual_memory_usage()
+        swap_memory_usage = self.get_swap_memory_usage()
+        return [Metric('memory_usage', virtual_memory_usage, int(time.time())),
+                Metric('swap_memory_usage', swap_memory_usage, int(time.time()))]
+
+
+class DiskMonitor(Monitor):
     def get_disk_usage(self, partition):
         usage = os.statvfs(partition)
         total = usage.f_blocks * usage.f_frsize
@@ -76,6 +110,12 @@ class Monitor:
             'percent': percent,
         }
 
+    def record_stats(self):
+        disk_usage = self.get_disk_usage('/')
+        return [Metric('disk_usage', disk_usage, int(time.time()))]
+
+
+class ProcessMonitor(Monitor):
     def get_execution_time(self):
         return time.time() - self.start_time
 
@@ -93,36 +133,40 @@ class Monitor:
     def get_current_thread_usage(self):
         return threading.active_count()
 
+    def record_stats(self):
+        process_info = self.get_process_info()
+        total_thread_usage = self.get_total_thread_usage()
+        current_thread_usage = self.get_current_thread_usage()
+        return [Metric('process_info', process_info, int(time.time())),
+                Metric('execution_time', self.get_execution_time(), int(time.time())),
+                Metric('total_thread_usage', total_thread_usage, int(time.time())),
+                Metric('current_thread_usage', current_thread_usage, int(time.time()))]
+
+
+class CombinedMonitor:
+    def __init__(self):
+        self.cpu_monitor = CPUMonitor()
+        self.memory_monitor = MemoryMonitor()
+        self.disk_monitor = DiskMonitor()
+        self.process_monitor = ProcessMonitor()
+
     def stop(self):
-        self.stop_flag = True
-        timeout = 2
-        self.cpu_usage_thread.join(timeout)
+        self.cpu_monitor.stop()
+        self.memory_monitor.stop()
+        self.disk_monitor.stop()
+        self.process_monitor.stop()
 
-    def is_thread_alive(self):
-        return self.cpu_usage_thread.is_alive()
+    def __enter__(self):
+        return self
 
-    def __del__(self):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
 
 
-class TestedAppMonitor(Monitor):
+class TestedAppMonitor(CombinedMonitor):
     def __init__(self, path):
         self.path = path
         super().__init__()
 
     def get_app_size(self):
         return os.path.getsize(self.path)
-
-    def get_tested_app_thread_usage(self):
-        service_threads = [self.cpu_usage_thread]
-        service_threads_num = len(service_threads)
-        return self.get_current_thread_usage() - service_threads_num
-
-    def get_line_count(self, file_type='py'):
-        line_count = 0
-        for root, dirs, files in os.walk(self.path):
-            for file in fnmatch.filter(files, f'*.{file_type}'):
-                with open(os.path.join(root, file), 'r') as f:
-                    lines = f.readlines()
-                    line_count += len(lines)
-        return line_count
